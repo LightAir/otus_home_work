fffe8c3fa73f82a101f134b3c0f2a6c715095eb1
LightAir <xsudo@yandex.ru>
2022-06-09 00:50:37 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +18,34 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	environment := make(Environment)
+
+	for _, f := range files {
+		file, err := os.Open(dir + "/" + f.Name())
+		if err != nil {
+			return nil, err
+		}
+		defer file.Close()
+
+		reader := bufio.NewReader(file)
+		line, _ := reader.ReadString('\n')
+		line = strings.TrimRight(line, "\t\n\r ")
+		line = strings.ReplaceAll(line, string(rune(0x00)), "\n")
+
+		env := EnvValue{
+			Value:      line,
+			NeedRemove: len(line) == 0,
+		}
+
+		name := strings.ReplaceAll(f.Name(), "=", "")
+
+		environment[name] = env
+	}
+
+	return environment, nil
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,44 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("simple", func(t *testing.T) {
+		env, err := ReadDir("testdata/env_custom")
+		require.NotNil(t, env, "env can't be nil")
+		require.Nilf(t, err, "err must be nil")
+
+		expectedEnvs := Environment{
+			"BAR":          EnvValue{Value: "bar", NeedRemove: false},
+			"EMPTY":        EnvValue{Value: "", NeedRemove: true},
+			"FOO":          EnvValue{Value: "   foo\nwith new line", NeedRemove: false},
+			"REMOVEEQUALS": EnvValue{Value: "remove equals from name", NeedRemove: false},
+			"TAB_CHECK":    EnvValue{Value: "\"next tabs\"", NeedRemove: false},
+			"UNSET":        EnvValue{Value: "", NeedRemove: true},
+		}
+
+		require.EqualValuesf(t, expectedEnvs, env, "Expected: %v, got %v", "asd", env)
+	})
+
+	t.Run("bad dir", func(t *testing.T) {
+		env, err := ReadDir("folder_not_found")
+		errMsg := "open folder_not_found: no such file or directory"
+
+		require.Nilf(t, env, "Expected: nil, got %v", env)
+		require.NotNilf(t, err, "Expected: %v, got nil", errMsg)
+		require.EqualErrorf(t, err, errMsg, "Error should be: %v, got: %v", errMsg, err)
+	})
+
+	t.Run("folder contains bad files", func(t *testing.T) {
+		env, err := ReadDir("/dev/block")
+		errMsg := "permission denied"
+
+		require.Nilf(t, env, "Expected: nil, got %v", env)
+		require.NotNilf(t, err, "Expected: %v, got nil", errMsg)
+		require.ErrorContains(t, err, errMsg, "Error should contains: %v, got: %v", errMsg, err)
+	})
 }
